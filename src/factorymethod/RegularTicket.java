3a897a30bb50efb1e3bf1ad0a0040e82ff66da44
avouane <avouane7@gmail.com>
2026-09-22 14:10:34 +0500

    add RegularTicket class

--- src/factorymethod/RegularTicket.java
+++ src/factorymethod/RegularTicket.java
@@ -1,4 +1,14 @@
 package factorymethod;
 
-public class RegularTicket {
+public class RegularTicket implements Ticket {
+    @Override
+    public double getPrice() {
+        return 10.0;
+    }
+
+    @Override
+    public String describe() {
+        return "Regular ticket - standard seating, no extra perks.";
+    }
 }
+
