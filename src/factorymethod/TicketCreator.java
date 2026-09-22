e0667d7bffd702b43470625a4b37ddb5e1744688
avouane <avouane7@gmail.com>
2026-09-22 14:12:56 +0500

    add TicketCreator class

--- src/factorymethod/TicketCreator.java
+++ src/factorymethod/TicketCreator.java
@@ -1,4 +1,14 @@
 package factorymethod;
 
-public class TicketCreator {
+public abstract class TicketCreator {
+
+    protected abstract Ticket createTicket();
+
+    public void sellTicket(String movieName) {
+        Ticket ticket = createTicket();
+        System.out.println("Selling ticket for \"" + movieName + "\"");
+        System.out.println(ticket.describe());
+        System.out.println("Price: $" + ticket.getPrice());
+        System.out.println("-----");
+    }
 }
