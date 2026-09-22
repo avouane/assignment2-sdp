09a6b173afb33ba50263d7eb66fa7c1345154d0c
avouane <avouane7@gmail.com>
2026-09-22 14:15:09 +0500

    add VipTicket class

--- src/factorymethod/VipTicket.java
+++ src/factorymethod/VipTicket.java
@@ -1,4 +1,13 @@
 package factorymethod;
 
-public class VipTicket {
+public class VipTicket implements Ticket {
+    @Override
+    public double getPrice() {
+        return 25.0;
+    }
+
+    @Override
+    public String describe() {
+        return "VIP ticket - reclining seat, free popcorn, priority entrance.";
+    }
 }
