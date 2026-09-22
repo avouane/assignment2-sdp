30a760b9fe650a600bc844d38b39d98bddcc6e45
avouane <avouane7@gmail.com>
2026-09-22 14:37:55 +0500

    add VipSnack and fill

---
+++ src/abstractfactory/VipSnack.java
@@ -0,0 +1,8 @@
+package abstractfactory;
+
+public class VipSnack implements Snack {
+    @Override
+    public String getSnackDescription() {
+        return "Gourmet popcorn combo with a complimentary drink";
+    }
+}
