93084dc8cdef1629c53e7effd67697032cd9eec7
avouane <avouane7@gmail.com>
2026-09-22 14:21:26 +0500

    add factoryMethod

--- src/factorymethod/factoryMethod.java
+++ src/factorymethod/factoryMethod.java
@@ -1,4 +1,15 @@
 package factorymethod;
 
 public class factoryMethod {
+    public static void main(String[] args) {
+        TicketCreator[] creators = {
+                new RegularTicketCreator(),
+                new VipTicketCreator(),
+                new StudentTicketCreator()
+        };
+
+        for (TicketCreator creator : creators) {
+            creator.sellTicket("Dune: Part Three");
+        }
+    }
 }
