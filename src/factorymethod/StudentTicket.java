3742fc32d091cb182d6afb0471126900ec1e8c93
avouane <avouane7@gmail.com>
2026-09-22 14:19:32 +0500

    add StudentTicket class

--- src/factorymethod/StudentTicket.java
+++ src/factorymethod/StudentTicket.java
@@ -1,4 +1,13 @@
 package factorymethod;
 
-public class StudentTicket {
+public class StudentTicket implements Ticket {
+    @Override
+    public double getPrice() {
+        return 6.0;
+    }
+
+    @Override
+    public String describe() {
+        return "Student ticket - discounted price, valid student ID required.";
+    }
 }
